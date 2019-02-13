65a30e7055ba4f8bddec824824aac7212503a149
Allex86 <38761034+Allex86@users.noreply.github.com>
2019-02-13 19:31:44 +1000

    upd

--- 14.php
+++ 14.php
@@ -7,20 +7,20 @@
 // частный случай для окрудностей с центрами в положительной части оси координат
 function hasCollision(array $circle, array $circlesLists): bool
 {
-    $tx = $circle[0];
-    $ty = $circle[1];
-    $tradius = $circle[2];
+    define("CENTER_CIRCLE_X", $circle[0]);
+    define("CENTER_CIRCLE_Y", $circle[1]);
+    define("CENTER_CIRCLE_R", $circle[2]);
 
     foreach ($circlesLists as $circleList) {
         // приравниваем тестируемую окружность к началу координат
-        $x = $circleList[0] - $tx;
-        $y = $circleList[1] - $ty;
+        $x = $circleList[0] - CENTER_CIRCLE_X;
+        $y = $circleList[1] - CENTER_CIRCLE_Y;
         $r = $circleList[2];
         // получаем прямоугольный треугольник с катетами $x и $y
         // вычисляем расстояние между центрами окружностей, равное гипотенузе
         $dr = sqrt(pow($x, 2) + pow($y, 2));
         // если сумма радиусов больше расстояния между центрами - окрудности пересекаются
-        if (($tradius + $r) >= $dr) {
+        if ((CENTER_CIRCLE_R + $r) >= $dr) {
             echo "Пересечение окружностей найдено!";
         }
     }
